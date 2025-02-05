bf4a3b267348cbc4e3dd3acdaf2f7257b7ef1856
mohamed665 <60930555+mohamed665@users.noreply.github.com>
2025-02-05 19:37:30 +0100

    new version all products

--- controllers/allproducts.controller.js
+++ controllers/allproducts.controller.js
@@ -1,4 +1,22 @@
+const Product = require("../models/product.model");
 
-exports.getPageallproducts=(req,res,next)=>{
-    res.render('allproducts',{ user: req.session.user})
-}
+exports.getPageallproducts = (req, res, next) => {
+    Product.getAllProducts()
+        .then(products => {
+            // Group products by category
+            const productsByCategory = {};
+            products.forEach(product => {
+                const categoryName = product.category ? product.category.name : "Uncategorized";
+                if (!productsByCategory[categoryName]) {
+                    productsByCategory[categoryName] = [];
+                }
+                productsByCategory[categoryName].push(product);
+            });
+
+            res.render("allproducts", { user: req.session.user, productsByCategory });
+        })
+        .catch(err => {
+            console.error("Error fetching products:", err);
+            res.status(500).send("Error loading products");
+        });
+};
